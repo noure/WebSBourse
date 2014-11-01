9ed2be6d48b9b3bc087d440ac085f520c1420865
noure <lnureddine@gmail.com>
2014-11-01 20:55:40 +0000

    web service bourse avec JAXB

---
+++ src/model/Cotation.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.sql.Date;
+
+public class Cotation {
+	private String codeSociete;
+	private String dateCotation;
+	private double valAction;
+
+	public String getCodeSociete() {
+		return codeSociete;
+	}
+
+	public void setCodeSociete(String codeSociete) {
+		this.codeSociete = codeSociete;
+	}
+
+	public String getDateCotation() {
+
+		return dateCotation;
+	}
+
+	public void setDateCotation(String dateCotation) {
+
+		this.dateCotation = dateCotation;
+	}
+
+	public double getValAction() {
+		return valAction;
+	}
+
+	public void setValAction(double valAction) {
+		this.valAction = valAction;
+	}
+}
